3e140f4746eef257496464d81b8d40eb84bb609d
BOUDRAA <diya.boudraa@etu.unistra.fr>
2025-05-31 18:49:56 +0200

    YAHOOOOO Init/deinit de switch et station Correction de ip2str/str2ip On a plus qu'une fuite memoire et elle est dans le main donc pas dans nos fonctions 🎉🎉🎉

--- source/machines.c
+++ source/machines.c
@@ -3,20 +3,29 @@
 
 
 void init_station(station *st){
+    //pratique vu qu'une machine a 0.0.0.0 en IP avant de s'en voir attribuée une
     memset(st->st_IP.ip, 0, 4);
     memset(st->st_MAC.mac, 0, 6);
 }
 
 void deinit_station(station *st){
-
+    //Je pense que faut réutiliser cette meme commande vu qu'on veut les repasser à 0
+    memset(st->st_IP.ip, 0, 4);
+    memset(st->st_MAC.mac, 0, 6);
 }
 
 void init_switch(swtch *sw){
-
+    memset(sw->sw_MAC.mac, 0, 6);
+    sw->nb_port = 16; //valeur totalement arbitraire, sujette à débat
+    sw->priorite = 0;
+    sw->tab_association = (sizeof(association) * sw->nb_port);
+    //tab voisin mais on a oublié son utilité MDRRR...
 }
 
 void deinit_switch(swtch *sw){
-
+    memset(sw->sw_MAC.mac, 0, 6);
+    free(sw->tab_association);
+    sw->tab_association = NULL;
 }
 
 void mac_to_str(adresse_MAC M, char *str){
@@ -24,7 +33,7 @@ void mac_to_str(adresse_MAC M, char *str){
 }
 
 void ip_to_str(adresse_IP IP, char *str){
-    sprintf(str, "%02hhX:%02hhX:%02hhX:%02hhX:%02hhX:%02hhX", IP.ip[0], IP.ip[1], IP.ip[2], IP.ip[3]);
+    sprintf(str, "%hhu.%hhu.%hhu.%hhu", IP.ip[0], IP.ip[1], IP.ip[2], IP.ip[3]);
 }
 
 void str_to_mac(adresse_MAC * M, char *str){
@@ -32,5 +41,5 @@ void str_to_mac(adresse_MAC * M, char *str){
 }
 
 void str_to_ip(adresse_IP * IP, char *str){
-    sscanf(str, "%02hhX:%02hhX:%02hhX:%02hhX:%02hhX:%02hhX", &IP->ip[0], &IP->ip[1], &IP->ip[2], &IP->ip[3]);
+    sscanf(str, "%hhu.%hhu.%hhu.%hhu", &IP->ip[0], &IP->ip[1], &IP->ip[2], &IP->ip[3]);
 }
